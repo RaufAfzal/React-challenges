51a95868dab3f868c7e9e901211e13612e704428
Rauf Afzal <raufafzal453@gmail.com>
2024-08-05 22:58:18 +0500

    use reducer implementation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { useReducer } from 'react';
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'Increment' :
+      return {...state , count: state.count+1};
+    case 'Decrement' :
+      return {...state , count: state.count-1};
+    case 'UserInputValue' :
+      return {...state , userInput: action.payload};
+    case 'tgColor' :
+      return {...state , color: false}
+    default:
+      throw new Error();
+  }
+}
+
+const ACTION = {
+  INCREMENT: 'Increment',
+  DECREMENT: 'Decrement', 
+  USERINPUTVALUE: 'UserInputValue',
+  TGCOLOR: 'tgColor'
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, {count: 0 , userInput: '', color: true})
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{color: state.color ? "#9b870c" : "Black"}}>
+      <input type="text"
+        placeholder='Enter an input'
+        value = {state.userInput}
+        onChange={(e) => dispatch({type: ACTION.USERINPUTVALUE, payload: e.target.value})}
+       /> <br></br>
+       <br></br>
+       {state.count}
+       <button onClick={() => dispatch({type: ACTION.INCREMENT})}>+</button> 
+       <button onClick={() => dispatch({type: ACTION.DECREMENT})}>-</button>
+       <button onClick={() => dispatch({type: ACTION.TGCOLOR})}>Color</button> <br></br><br></br>
+
+       {state.userInput}
     </div>
   );
 }
