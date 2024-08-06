e5562ec06d296650c19c3e281e41fe7b3bd96713
Rauf Afzal <raufafzal453@gmail.com>
2024-08-06 14:31:56 +0500

    use layout effect challenge completed

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import logo from './logo.svg';
 import './App.css';
 
@@ -19,6 +20,33 @@ function App() {
         </a>
       </header>
     </div>
+=======
+import { useState, useRef, useLayoutEffect } from 'react';
+
+function App() {
+  const [number, setNumber] = useState(0);
+  const [sectionStyle, setSectionStyle] = useState({});
+  const sectionRef = useRef();
+
+  useLayoutEffect(() => {
+    const random = Math.floor(Math.random() * 500);
+
+    for (let i = 0; i <= 100000000; i++) {
+      if (i === 100000000) setSectionStyle({ paddingTop: `${random}px` })
+    }
+  }, [number])
+
+  return (
+    <main className="App">
+      <section ref={sectionRef} style={sectionStyle}>
+        <p>{number}</p>
+        <div>
+          <button onClick={() => setNumber(prev => prev - 1)}>-</button>
+          <button onClick={() => setNumber(prev => prev + 1)}>+</button>
+        </div>
+      </section>
+    </main>
+>>>>>>> Stashed changes
   );
 }
 
