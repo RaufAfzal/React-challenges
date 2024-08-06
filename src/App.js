19f008221a13d638f18484af680ca4348a83f8b2
Rauf Afzal <raufafzal453@gmail.com>
2024-08-06 14:32:12 +0500

    changes completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,3 @@
-<<<<<<< Updated upstream
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
 import { useState, useRef, useLayoutEffect } from 'react';
 
 function App() {
@@ -46,7 +23,6 @@ function App() {
         </div>
       </section>
     </main>
->>>>>>> Stashed changes
   );
 }
 
